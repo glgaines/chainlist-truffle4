67b55aa36097b77c6a654bad84b7dd502b6934c2
glgaines <glgaines@users.noreply.github.com>
2018-03-20 14:21:24 -0400

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -78,6 +78,7 @@ App = {
       }
       App.loading = false;
     }).catch(function(err) {
+      console.log("error caught here 81")
       console.error(err.message);
       App.loading = false;
     });
